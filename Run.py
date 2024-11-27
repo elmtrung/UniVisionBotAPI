c280debef6c8caaa2801fa31e36d5f4ceb9cef10
Hà Quang Trung <156428155+elmtrung@users.noreply.github.com>
2024-11-27 13:51:02 +0700

    Update API

--- Run.py
+++ Run.py
@@ -1,25 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+from bson import ObjectId
+from datetime import datetime
 from poe_api_wrapper import AsyncPoeApi
 import asyncio
 
 app = Flask(__name__)
 CORS(app)
 
-# tokens = {
-#     'p-b': 'ToDAkUbHxfsHWu1fGpz_cA%3D%3D',
-#     'p-lat': 'afGkUC17qGhaRPWctFWPOT7Ryg7nPvCemYA8iICo7g%3D%3D',
-# }
+client = MongoClient('mongodb://localhost:27017/')
+db = client['UniVisionBot']
+pending_conversations = db['PendingConversation']
+conversations = db['Conversation']
+messages = db['Message']
+
+tokens = {
+    'p-b': 'ToDAkUbHxfsHWu1fGpz_cA%3D%3D',
+    'p-lat': 'afGkUC17qGhaRPWctFWPOT7Ryg7nPvCemYA8iICo7g%3D%3D',
+}
 
 # tokens = {
 #     'p-b': 'ToDAkUbHxfsHWu1fGpz_cA%3D%3D',
 #     'p-lat': 'KVhlmpJfKhiMzrrUZfeuXHam%2Bznbd9nmls3BmDnLfw%3D%3D',
 # }
 
-tokens = {
-    'p-b': 'ToDAkUbHxfsHWu1fGpz_cA%3D%3D',
-    'p-lat': 'KVhlmpJfKhiMzrrUZfeuXHam%2Bznbd9nmls3BmDnLfw%3D%3D',
-}
+# tokens = {
+#     'p-b': 'ToDAkUbHxfsHWu1fGpz_cA%3D%3D',
+#     'p-lat': 'KVhlmpJfKhiMzrrUZfeuXHam%2Bznbd9nmls3BmDnLfw%3D%3D',
+# }
 
 
 async def send_message_to_bot(message, chatId=None, chatCode=None):
@@ -30,6 +39,36 @@ async def send_message_to_bot(message, chatId=None, chatCode=None):
         chatId = chunk.get("chatId", chatId)
         chatCode = chunk.get("chatCode", chatCode)
     return response, chatId, chatCode
+
+def add_conversation(user_Id):
+    conversation = {
+        "consutant_Id": '672214299c167656b2dc0d5e',
+        "user_Id": user_Id,
+        "created_at": datetime.utcnow()
+    }
+    result = conversations.insert_one(conversation)
+    return result.inserted_id
+
+def add_pending_conversation(status, user_Id):
+    pending_conversation = {
+        "conversation_id": add_conversation(user_Id),
+        "Status": status,
+        "user_Id": user_Id,
+        "created_at": datetime.utcnow()
+    }
+    pending_conversations.insert_one(pending_conversation)
+    return pending_conversation["conversation_id"]
+
+def add_message(conversation_Id, message, sender):
+    message_doc = {
+        "status": 0,
+        "message": message,
+        "conversation_id": conversation_Id,
+        "sender": sender,
+        "receiver": "672214299c167656b2dc0d5e",
+        "created_at": datetime.utcnow()
+    }
+    messages.insert_one(message_doc)
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -47,6 +86,19 @@ def send_message():
         'chatId': chatId,
         'chatCode': chatCode
     })
+
+@app.route('/add_pending_conversation', methods=['POST'])
+def api_add_pending_conversation():
+    data = request.json
+    conversation_id = add_pending_conversation(data['status'], data['user_Id'])
+    return jsonify({"conversation_id": str(conversation_id)})
+
+@app.route('/add_message', methods=['POST'])
+def api_add_message():
+    data = request.json
+    print("data:", data)
+    add_message(data['conversationId'], data['message'], data['sender'])
+    return jsonify({"status": "success"})
 
 if __name__ == '__main__':
     app.run(debug=True)
